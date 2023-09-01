f5ce083b9a466e77945c980fde10b6d9d1d3ce77
Indika <45280177+IndiW@users.noreply.github.com>
2023-08-31 22:48:52 -0400

    remove extra pages and add social template

--- homepage/src/App.tsx
+++ homepage/src/App.tsx
@@ -1,54 +1,9 @@
 import { useState } from 'react'
 import BaseLandingPage from './pages/BaseLandingPage/BaseLandingPage'
-import BriefcasePage from './pages/BriefcasePage'
-import OpenOverlayButton from './components/OpenOverlayButton'
-import { usePages } from './hooks/usePages'
-import ControlsOverlay from './components/ControlsOverlay/ControlsOverlay'
-import GamersPage from './pages/GamersPage/GamersPage'
 export default function App() {
-    const [page, changePage] = usePages(2)
-    const [showControlsOverlay, setShowControlsOverlay] = useState(false)
-
-    const openOverlay = () => {
-        setShowControlsOverlay(true)
-    }
-
-    const closeOverlay = () => {
-        setShowControlsOverlay(false)
-    }
-
-    const currentPage = (page: number) => {
-        switch (page) {
-            case 0:
-                return <BaseLandingPage />
-            case 1:
-                return <BriefcasePage />
-            case 2:
-                return <GamersPage />
-            default:
-                return <BaseLandingPage />
-        }
-    }
-
-    const handleChangePage = (page: number) => {
-        const newPage = changePage(page)
-        setShowControlsOverlay(false)
-        return newPage
-    }
-
-    const controlsOverlay = showControlsOverlay ? (
-        <ControlsOverlay changePage={handleChangePage} handleCloseOverlay={closeOverlay} />
-    ) : (
-        <></>
-    )
-
     return (
         <div className='h-screen w-full'>
-            <div className='relative'>
-                {controlsOverlay}
-                {currentPage(page)}
-                <OpenOverlayButton openOverlay={openOverlay} />
-            </div>
+            <BaseLandingPage />
         </div>
     )
 }
--- homepage/src/pages/BaseLandingPage/BaseLandingPage.tsx
+++ homepage/src/pages/BaseLandingPage/BaseLandingPage.tsx
@@ -1,11 +1,25 @@
-// TODO add icons for tools
 export default function BaseLandingPage() {
     return (
-        <div className='flex h-screen flex-col items-center justify-center gap-y-5'>
-            <div className='text-center'>
-                <h1 className='text-3xl font-bold'>Indika Wijesundera</h1>
-                <h1 className='text-2xl'>Software Engineer</h1>
-                <h1 className='text-xl'>React, Nodejs, CDK</h1>
+        <div className='relative'>
+            <div
+                className='absolute'
+                style={{
+                    height: 50,
+                    width: 50,
+                    backgroundColor: '#bbb',
+                    borderRadius: '50%',
+                    display: 'inline-block',
+                    top: '30%',
+                    left: '5%',
+                }}
+            ></div>
+            <div className='bg-black' style={{ height: '10vh' }}></div>
+            <div className='flex h-3/5 flex-col  gap-y-5'>
+                <div className='pl-2 pt-10'>
+                    <h1 className='text-xl font-bold'>Indika Wijesundera</h1>
+                    <h1 className='text-base text-slate-500'>@SoftwareEngineer</h1>
+                    <h1 className='text-base'>React, Nodejs, CDK</h1>
+                </div>
             </div>
         </div>
     )
