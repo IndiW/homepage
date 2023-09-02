766e6c55796c0ad8c20741ce1a8e034c16a5ea8a
Indika <45280177+IndiW@users.noreply.github.com>
2023-09-02 14:54:11 -0400

    Add icons and first post

---
+++ homepage/src/components/Icons/types.ts
@@ -0,0 +1,5 @@
+export type IconProps = {
+    w: string
+    h: string
+    color: string
+}
--- homepage/src/pages/BaseLandingPage/BaseLandingPage.tsx
+++ homepage/src/pages/BaseLandingPage/BaseLandingPage.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { HeartIcon, LocationIcon } from '../../components/Icons'
 
 export function ProfileIcon() {
     return (
@@ -43,9 +44,10 @@ export function Post(props: PostProps) {
                         </h2>
                     </span>
                     <p>{content}</p>
-                    <span className='flex flex-row items-center'>
-                        <p className='text-sm text-slate-500'>Likes: {metadata.likes}</p>
-                    </span>
+                    <div className='flex flex-row items-center gap-2'>
+                        <HeartIcon w={'15px'} h={'15px'} color={'#64748b'} />
+                        <p className='text-sm text-slate-500'>{metadata.likes}</p>
+                    </div>
                 </div>
             </div>
         </div>
@@ -62,10 +64,15 @@ export function TabButton(props: TabButtonProps) {
 
     return (
         <div>
-            <button onClick={handleClick} className={'p-2 hover:bg-slate-100'}>
+            <button
+                onClick={handleClick}
+                className={`px-2 py-1 hover:bg-slate-100 ${
+                    isActive ? 'text-black' : 'text-slate-500'
+                }`}
+            >
                 {label}
             </button>
-            <hr className={`${isActive ? 'h-1 bg-black' : ' bg-slate-500'}`} />
+            <hr className={`${isActive ? 'h-1 bg-black' : 'invisible'}`} />
         </div>
     )
 }
@@ -73,17 +80,51 @@ export function TabButton(props: TabButtonProps) {
 const tabLabels = ['All', 'Projects', 'Posts'] as const
 type TabLabel = (typeof tabLabels)[number]
 
+type PostType = 'project' | 'text'
+type PostData = {
+    type: PostType
+    date: string
+    metadata: PostMetadata
+    content: string
+}
+
+const postsData: Array<PostData> = [
+    {
+        type: 'text',
+        date: 'Sept 2, 2023',
+        metadata: {
+            likes: 20,
+            shares: 20,
+        },
+        content: 'First post',
+    },
+]
+
+const postTypeToTabNameMap: Record<PostType, TabLabel> = {
+    text: 'Posts',
+    project: 'Projects',
+}
+
 export default function BaseLandingPage() {
     const [currentTab, setCurrentTab] = useState<TabLabel>('All')
+    const [posts, setPosts] = useState<Array<PostData>>(postsData)
+
     const handleTabClick = (newLabel: TabLabel) => {
-        return () => setCurrentTab(newLabel)
+        return () => {
+            if (newLabel === 'All') {
+                setPosts(postsData)
+            } else {
+                setPosts(postsData.filter((post) => newLabel === postTypeToTabNameMap[post.type]))
+            }
+            setCurrentTab(newLabel)
+        }
     }
     const profileName = 'Indika Wijesundera'
     const profileHandle = '@SoftwareEngineer'
 
     return (
         <div className='relative'>
-            <div className='absolute' style={{ top: '16%', left: '2%' }}>
+            <div className='absolute' style={{ top: '60px', left: '8px' }}>
                 <ProfileIcon />
             </div>
             <div className='bg-black' style={{ height: '10vh' }}></div>
@@ -92,30 +133,43 @@ export default function BaseLandingPage() {
                     <h1 className='text-xl font-bold'>{profileName}</h1>
                     <h1 className='text-base text-slate-500'>{profileHandle}</h1>
                     <h1 className='text-base'>React, Nodejs, CDK</h1>
-                    <h1 className='text-base text-slate-500'>Toronto, CA</h1>
+                    <div className='flex items-center'>
+                        <LocationIcon w={'18x'} h={'18px'} color={'#64748b'} />
+                        <h1 className='text-base text-slate-500'>Toronto, CA</h1>
+                    </div>
                     <h1 className='text-base text-slate-500'>
                         <span className='font-bold text-black'>500</span> Trying to recruit
                     </h1>
                 </div>
-                <div className='row-auto flex w-screen gap-8'>
-                    {tabLabels.map((label) => {
+                <div>
+                    <div className='row-auto flex w-screen gap-8'>
+                        {tabLabels.map((label) => {
+                            return (
+                                <TabButton
+                                    isActive={label === currentTab}
+                                    label={label}
+                                    handleClick={handleTabClick(label)}
+                                />
+                            )
+                        })}
+                    </div>
+                    <hr className='w-screen' />
+                </div>
+                <div className='flex flex-col gap-4'>
+                    {posts.map((data) => {
                         return (
-                            <TabButton
-                                isActive={label === currentTab}
-                                label={label}
-                                handleClick={handleTabClick(label)}
-                            />
+                            <>
+                                <Post
+                                    date={data.date}
+                                    content={data.content}
+                                    profileName={profileName}
+                                    profileHandle={profileHandle}
+                                    metadata={data.metadata}
+                                />
+                                <hr className='w-screen' />
+                            </>
                         )
                     })}
-                </div>
-                <div>
-                    <Post
-                        date={'Sept 2, 2023'}
-                        content='First post'
-                        profileName={profileName}
-                        profileHandle={profileHandle}
-                        metadata={{ likes: 20, shares: 25 }}
-                    />
                 </div>
             </div>
         </div>
